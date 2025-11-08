75a09a06cd71e347183c128dae0bad571f5fef69
riteshd26 <riteshdangre.rd@gmail.com>
2025-11-08 13:38:16 +0530

    Rename S3 bucket for EKS stategood

--- eks-install/backend/main.tf
+++ eks-install/backend/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "demo-terraform-eks-state-s3-bucket"
+  bucket = "rits-demo-terraform-eks-state-s3-bucket"
 
   lifecycle {
     prevent_destroy = false
